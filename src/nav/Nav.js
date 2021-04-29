be30e3b26c4a91c84d46b871a2b71eee7f8f7fa9
RamanSlinka <romuch1986@gmail.com>
2021-04-29 21:23:23 +0300

    new project - my portfolio :)

---
+++ src/nav/Nav.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import s from './nav.module.css'
+
+function Nav() {
+    return (
+        <div className={s.nav}>
+            <a href="Главная">Главная</a>
+            <a href="Скилы">Скилы</a>
+            <a href="Проекты">Проекты</a>
+            <a href="Контакты">Контакты</a>
+
+        </div>
+    );
+}
+
+export default Nav;
