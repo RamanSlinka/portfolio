d1a0beb34dae413c662e31df0e34769edfaa0ee5
RamanSlinka <romuch1986@gmail.com>
2021-06-11 22:54:08 +0300

    work on main component and add scss

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,21 +1,29 @@
 import React from 'react'
-import s from './Main.module.css'
+import s from './Main.module.scss'
 import styleContainer from '../../src/common/styles/Container.module.css'
-
+import myPhotoImage from './../assets/image/myPhoto.jpg'
 
 function Main() {
+
+    const myPhoto = {
+        backgroundImage: `url(${myPhotoImage})`,
+    };
+
     return (
         <div className={s.mainBlock}>
             <div className={styleContainer.container}>
                 <div className={s.text}>
                     <span>Hi There</span>
                     <h1>
-                        I am Raman Slinka
+                        I am &nbsp;
+                        <span>   Raman Slinka</span>
                     </h1>
                     <p>Frontend Developer</p>
                 </div>
-                <div className={s.photo}>
-                    <img src="###" alt="my best photo:)"/>
+                <div className={s.photo}
+                     style={myPhoto}
+                >
+
                 </div>
             </div>
         </div>
