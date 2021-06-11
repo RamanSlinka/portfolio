6edcd9acc7bf9a09f9c74ab25c39689fa11776a5
RamanSlinka <romuch1986@gmail.com>
2021-06-12 01:06:06 +0300

    start work on skills component and add scss

--- src/skills/Skills.js
+++ src/skills/Skills.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import s from './Skills.module.css'
+import s from './Skills.module.scss'
 import styleContainer from '../common/styles/Container.module.scss'
 import Skill from "./skill/Skill";
 import Title from "../common/Components/Title/Title";
