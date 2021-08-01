44770d65c58174828472cb611ffd6bd7dd644388
RamanSlinka <romuch1986@gmail.com>
2021-08-01 22:24:58 +0300

    add handlerForm add EmailJS

--- src/contacts/Contacts.js
+++ src/contacts/Contacts.js
@@ -2,25 +2,51 @@ import React from 'react'
 import s from './Contacts.module.scss'
 import styleContainer from '../common/styles/Container.module.scss'
 import Title from "../common/Components/Title/Title";
+import emailjs from 'emailjs-com';
 
-function Contacts () {
+
+function Contacts() {
+
+    function sendEmail(e) {
+        e.preventDefault();
+        emailjs.sendForm('service_4e1ddah', 'template_9x2ipjy',
+            e.target, 'user_mnUCEEr4By9wSIzyUVj9X')
+            .then((result) => {
+                console.log(result.text);
+            }, (error) => {
+                console.log(error.text);
+            });
+        e.target.reset();
+    }
+
+
     return (
         <div id='contact' className={s.contactsBlock}>
             <div className={`${styleContainer.container} ${s.contacts}`}>
                 <Title text={'Contact'}/>
-                    <form action="URL" className={s.form}>
-                        <input type="text" className={s.input}
-                        placeholder={'name'}
-                        />
-                        <input type="email" className={s.input}
-                        placeholder={'E-mail'}
-                        />
-                        <textarea placeholder={'Enter your question'} />
-                    </form>
-                <button type="submit" className={styleContainer.button}><span className={styleContainer.descriptionBtn}>Send message</span></button>
+                <form onSubmit={sendEmail} className={s.form}>
+                    <input type="text" className={s.input}
+                           placeholder={'name'}
+                           name='to_name'
+                    />
+                    <input type="email" className={s.input}
+                           placeholder={'E-mail'}
+                           name='from_name'
+                    />
+                    <input type="text" className={s.input}
+                           placeholder={'subject'} name='from_name'
+                    />
+                    <textarea placeholder={'Enter your question'}
+                    name='message'
+                    />
+                    <button type="submit" className={styleContainer.button}><span className={styleContainer.descriptionBtn}>Send message</span>
+                    </button>
+                </form>
+
             </div>
 
         </div>
     )
 }
+
 export default Contacts
